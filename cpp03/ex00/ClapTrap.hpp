60a89665e3e97a7cb71b946ee9aa7a0ca56dff07
mucahitcakmak <mucoox18@gmail.com>
2023-11-20 12:16:41 +0000

    Cpp03 - ex00 completed.

---
+++ cpp03/ex00/ClapTrap.hpp
@@ -0,0 +1,20 @@
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
+
+#include <iostream>
+
+class ClapTrap {
+    private:
+        std::string name;
+        int     hitPoints;
+        int     energyPoints;
+        int     attackDamage;
+    public:
+        ClapTrap();
+        ClapTrap(std::string name);
+        void attack(const std::string& target);
+        void takeDamage(unsigned int amount);
+        void beRepaired(unsigned int amount);       
+};
+
+#endif
