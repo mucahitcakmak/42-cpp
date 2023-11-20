5245879a7aaae694438cff0419e6fb702ac8f7f3
mucahitcakmak <mucoox18@gmail.com>
2023-11-20 12:15:51 +0000

    Cpp01 - ex04 completed.

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -6,24 +6,34 @@ using std::endl;
 using std::find;
 using std::string;
 
-// --------------- Tamamlanmadı !! ---------
-
-int main(int ac, char **argv) {
+int main(int ac, char *argv[]) {
     if (ac != 4) {
         cout << "You must enter 4 arguments." << endl;
         return (1);
     }
 
+    std::ifstream infile(argv[1]);
+    std::ofstream outfile((string(argv[1]) + ".replace").c_str());
+
     string s1 = argv[2];
     string s2 = argv[3];
 
-    std::ifstream infile(argv[1]);
+    int    findIn;
     string lines;
     string line;
     if (infile.is_open()) {
         while (std::getline(infile, line)) {
-            line.find(s1);
+            findIn = line.find(s1);
+            while (findIn != -1) {
+                findIn = line.find(s1);
+                if (findIn != -1) {
+                    line.erase(findIn, s1.length());
+                    line.insert(findIn, s2);
+                }
+            }
             lines += line + '\n';
         }
+        infile.close();
+        outfile << lines;
     }
 }
