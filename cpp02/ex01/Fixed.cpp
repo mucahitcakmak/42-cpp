0b4314487870d906cc60c82bdfa2509d4623f9aa
Mücahit Çakmak <126570487+mucahitcakmak@users.noreply.github.com>
2023-12-25 17:31:11 +0000

    Cpp02 - ex01 completed.

--- cpp02/ex01/Fixed.cpp
+++ cpp02/ex01/Fixed.cpp
@@ -5,6 +5,16 @@ Fixed::Fixed() {
     this->value = 0;
 }
 
+Fixed::Fixed(const int newValue) {
+    std::cout << "Int constructor called." << std::endl;
+    this->value = newValue << this->fixed;
+}
+
+Fixed::Fixed(const float newValue) {
+    std::cout << "Float constructor called." << std::endl;
+    this->value = (int)roundf(newValue * pow(2, this->fixed));
+}
+
 Fixed::Fixed(const Fixed &cpy) {
     std::cout << "Copy constructor called." << std::endl;
     *this = cpy;
@@ -14,8 +24,16 @@ Fixed& Fixed::operator = (const Fixed &cpy) {
     if (this == &cpy)
         return (*this);
     std::cout << "Copy assignment operator called." << std::endl;
-    this->setRawBits(cpy.getRawBits());
+    this->setRawBits(cpy.value);
     return (*this);
+}
+
+int Fixed::toInt(void) const {
+    return ((int)this->value >> this->fixed);
+}
+
+float Fixed::toFloat(void) const {
+    return ((float)this->value / (1 << this->fixed));
 }
 
 int Fixed::getRawBits() const {
@@ -29,4 +47,10 @@ void Fixed::setRawBits(int const raw) {
 
 Fixed::~Fixed() {
     std::cout << "Destructor called." << std::endl;
+}
+
+std::ostream &operator << (std::ostream &output, const Fixed &fixed)
+{
+    output << fixed.toFloat();
+    return output;
 }
