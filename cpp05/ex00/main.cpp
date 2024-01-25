6532ec819b1ec06e17c3153daf0a5872581c697b
Mücahit Çakmak <126570487+mucahitcakmak@users.noreply.github.com>
2024-01-25 10:59:31 +0000

    Cpp05 - ex00 completed.

---
+++ cpp05/ex00/main.cpp
@@ -0,0 +1,19 @@
+#include "Bureaucrat.hpp"
+
+int main() {
+    Bureaucrat mucox("Muco", 1);
+    Bureaucrat esadx("Esad", 1);
+    Bureaucrat fikarmex("Akif", 151);
+    Bureaucrat bot1("Bot1", 0);
+    Bureaucrat bot2("Bot2", -12);
+    
+    std::cout << "----------" << std::endl;
+    std::cout << mucox.getName() << " - " << mucox.getGrade() << std::endl;
+    std::cout << std::endl;
+
+    esadx.increment();
+    mucox.decrement();
+    std::cout << esadx << std::endl;
+    std::cout << mucox << std::endl;
+    std::cout << "----------" << std::endl;
+}
