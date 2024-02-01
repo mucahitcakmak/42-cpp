a8ae6b4867ee7316b823bc2020d01811e5ebcf75
Mücahit Çakmak <126570487+mucahitcakmak@users.noreply.github.com>
2024-02-01 10:59:20 +0000

    Cpp06 - ex00 completed.

--- cpp06/ex00/ScalarConverter.hpp
+++ cpp06/ex00/ScalarConverter.hpp
@@ -2,14 +2,24 @@
 #define SCALARCONVERTER_HPP
 
 #include <iostream>
+#include <iomanip>
+#include <sstream>
 
 class ScalarConverter {
-	private:
-		std::string type;
-		std::string type;
 	public:
 		ScalarConverter();
+		ScalarConverter(const ScalarConverter &cpy);
+		ScalarConverter &operator=(const ScalarConverter &cpy);
+		static void findType(std::string &arg);
 		~ScalarConverter();
 };
+
+void setType(const std::string &str);
+void nanCases(const std::string str);
+void castChar(std::string str);
+void castInt(std::string str);
+void castFloat(std::string str);
+void castDouble(std::string str);
+void nothingType();
 
 #endif
--- cpp06/ex00/main.cpp
+++ cpp06/ex00/main.cpp
@@ -1,5 +1,10 @@
 #include "ScalarConverter.hpp"
 
-int main() {
-	
+int main(int ac, char **av) {
+    if (ac != 2)
+        return (1);
+    ScalarConverter convert;
+
+    std::string arg = av[1];
+    convert.findType(arg);
 }
