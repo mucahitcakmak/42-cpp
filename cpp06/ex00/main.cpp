128f5283af2d61309f21060e4e81cb132152cb51
Mücahit Çakmak <126570487+mucahitcakmak@users.noreply.github.com>
2024-02-01 12:19:21 +0000

    Cpp06 - ex00 edited.

--- cpp06/ex00/main.cpp
+++ cpp06/ex00/main.cpp
@@ -3,8 +3,7 @@
 int main(int ac, char **av) {
     if (ac != 2)
         return (1);
-    ScalarConverter convert;
 
     std::string arg = av[1];
-    convert.findType(arg);
+    ScalarConverter::findType(arg);
 }
