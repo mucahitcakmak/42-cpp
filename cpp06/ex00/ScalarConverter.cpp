8b0b3695312782989e350a385f50fb44ffcbbeb4
mucahitcakmak <mucoox18@gmail.com>
2024-02-01 11:50:49 +0300

    Cpp06 - ex00 started.

---
+++ cpp06/ex00/ScalarConverter.cpp
@@ -0,0 +1,9 @@
+#include "ScalarConverter.hpp"
+
+ScalarConverter::ScalarConverter() {
+
+};
+
+ScalarConverter::~ScalarConverter() {
+	std::cout << "Destructor called" << std::endl;
+};
---
+++ cpp06/ex00/ScalarConverter.hpp
@@ -0,0 +1,15 @@
+#ifndef SCALARCONVERTER_HPP
+#define SCALARCONVERTER_HPP
+
+#include <iostream>
+
+class ScalarConverter {
+	private:
+		std::string type;
+		std::string type;
+	public:
+		ScalarConverter();
+		~ScalarConverter();
+};
+
+#endif
