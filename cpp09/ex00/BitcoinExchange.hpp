4334d2d0decffa5632e94829ca2fc61e23b0e9f2
Mücahit Çakmak <126570487+mucahitcakmak@users.noreply.github.com>
2024-06-04 09:06:59 +0000

    Cpp09 - ex00 completed.

---
+++ cpp09/ex00/BitcoinExchange.hpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <map>
+#include <string>
+#include <fstream>
+#include <cstdlib>
+#include <stdexcept>
+
+class BitcoinExchange {
+    private:
+        std::map<std::string, float> btc_data;
+        int min_year;
+    public:
+        void read_data();
+        void read_input(std::string input_file_name);
+        void check_line_and_print(std::string line);
+};
